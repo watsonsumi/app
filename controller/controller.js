5c0d0ee6a605cea562ca6248be70b39af3549aa4
Sumilda <34973025+watsonsumi@users.noreply.github.com>
2023-05-09 09:04:05 -0500

    cambios en controller

--- controller/controller.js
+++ controller/controller.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.status(200).json({ success: true, data: 'healthy!' });
+    res.send("Hola mundo ");
 });
 
 module.exports = router;
